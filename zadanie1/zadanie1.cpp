eeb34ff722a2bad535db0ca8ae967842d1802ddd
Ivabek33 <tiwanski@interia.pl>
2024-10-13 22:10:48 +0200

    utworzenie wezla oraz przypisanie go do poczatku listy

--- zadanie1/zadanie1.cpp
+++ zadanie1/zadanie1.cpp
@@ -20,9 +20,41 @@ public:
     // Konstruktor listy dwukierunkowej
     DoublyLinkedList() : head(nullptr), tail(nullptr) {}
 
+    // Dodawanie elementu na początku listy
+    void insertAtBeginning(int value) {
+        Node* newNode = new Node(value); // Tworzenie nowego węzła
+        if (head == nullptr) {           // Jeśli lista jest pusta
+            head = tail = newNode;       // Head i tail wskazują na nowy węzeł
+        }
+        else {
+            newNode->next = head;        // Nowy węzeł wskazuje na obecny head
+            head->prev = newNode;        // Obecny head wskazuje na nowy węzeł jako prev
+            head = newNode;              // Aktualizacja head do nowego węzła
+        }
+    }
+
+    // Wyświetlanie listy od początku
+    void displayForward() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            std::cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        std::cout << "nullptr" << std::endl;
+    }
 };
 
-int main()
-    {
-        return 0;
-    }
+int main() {
+    DoublyLinkedList list;
+
+    // Dodawanie elementów na początku listy
+    list.insertAtBeginning(10);
+    list.insertAtBeginning(20);
+    list.insertAtBeginning(30);
+
+    // Wyświetlanie listy od początku
+    std::cout << "Lista dwukierunkowa: ";
+    list.displayForward();
+
+    return 0;
+}
