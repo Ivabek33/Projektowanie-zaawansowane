d4214bd37de6b2bf3b4ae8d027038b7a9a60ce43
Ivabek33 <tiwanski@interia.pl>
2024-10-13 22:08:49 +0200

    dodanie klass oraz struktury

--- zadanie1/zadanie1.cpp
+++ zadanie1/zadanie1.cpp
@@ -1,7 +1,28 @@
 ﻿#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+// Struktura reprezentująca węzeł listy dwukierunkowej
+struct Node {
+    int data;       // Przechowywane dane
+    Node* prev;     // Wskaźnik na poprzedni węzeł
+    Node* next;     // Wskaźnik na następny węzeł
 
+    // Konstruktor węzła
+    Node(int value) : data(value), prev(nullptr), next(nullptr) {}
+};
+
+// Klasa reprezentująca listę dwukierunkową
+class DoublyLinkedList {
+private:
+    Node* head;     // Wskaźnik na pierwszy węzeł listy
+    Node* tail;     // Wskaźnik na ostatni węzeł listy
+
+public:
+    // Konstruktor listy dwukierunkowej
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+};
+
+int main()
+    {
+        return 0;
+    }
